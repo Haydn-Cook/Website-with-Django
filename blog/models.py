5eabe5c4ef8240543787d9f8489a5b94475e8b19
Haydn <chaydn114@gmail.com>
2023-08-29 12:03:59 +0200

    Added docstings

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Blog_Post(models.Model):
+    '''
+    This block of code creates a blog post form users,
+    also deletes all posts linked to a user if user acc is deleted
+    '''
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     blog_title = models.CharField(max_length=200)
     body = models.TextField()
